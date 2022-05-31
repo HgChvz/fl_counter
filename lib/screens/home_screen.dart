b94733b0bbb0a3616b053d04c85f4e345bdf867e
Hugo Chavez <hgchvz.dev@gmail.com>
2022-05-31 17:45:58 -0400

    Aumentamos el FloatingActionButton y se coloco estilo al Text

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,13 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
-  
+
   const HomeScreen({
     Key? key
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    TextStyle fontSize35 = const TextStyle(fontSize: 35);
 
     return Scaffold(
 
@@ -19,13 +21,19 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Center(
           child: Column(
-            children: const [
-              Text('Clicks counter:'),
-              SizedBox(height: 10,),
-              Text('10'),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Clicks counter:', style: fontSize35),
+              const SizedBox(height: 15, ),
+              Text('10', style: fontSize35),
             ],
           ),
       ), 
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.plus_one_rounded),
+        onPressed: () {},
+        
+      ),
     );
   }
   
